321931202d7ed52ed6be55ef12d00ca6ebad3cd2
Keith Garcia <Keith.Garcia444@gmail.com>
2020-07-13 14:14:54 -0400

    final for grading

--- tuneup.py
+++ tuneup.py
@@ -5,20 +5,36 @@
 Use the timeit and cProfile libraries to find bad code.
 """
 
-__author__ = "???"
+__author__ = "Keith Garcia, co-author was Ben McKenzie, we used the docs for python about Profiling"
 
 import cProfile
 import pstats
 import functools
+import io
+
+
+# def profile(func):
+#     """A cProfile decorator function that can be used to
+#     measure performance.
+#     """
+#     # Be sure to review the lesson material on decorators.
+#     # You need to understand how they are constructed and used.
+#     raise NotImplementedError("Complete this decorator function")
 
 
 def profile(func):
-    """A cProfile decorator function that can be used to
-    measure performance.
-    """
-    # Be sure to review the lesson material on decorators.
-    # You need to understand how they are constructed and used.
-    raise NotImplementedError("Complete this decorator function")
+    def inner(*args, **kwargs):
+        pr = cProfile.Profile()
+        pr.enable()
+        retval = func(*args, **kwargs)
+        pr.disable()
+        s = io.StringIO()
+        sortby = 'cumulative'
+        ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+        ps.print_stats()
+        print(s.getvalue())
+        return retval
+    return inner
 
 
 def read_movies(src):
@@ -31,18 +47,20 @@ def read_movies(src):
 def is_duplicate(title, movies):
     """Returns True if title is within movies list."""
     for movie in movies:
-        if movie.lower() == title.lower():
+        if movie == title:
             return True
     return False
 
 
+@profile
 def find_duplicate_movies(src):
     """Returns a list of duplicate movies from a src list."""
     movies = read_movies(src)
+    movies = [movie.lower() for movie in movies]
     duplicates = []
     while movies:
         movie = movies.pop()
-        if is_duplicate(movie, movies):
+        if movie in movies:
             duplicates.append(movie)
     return duplicates
 
